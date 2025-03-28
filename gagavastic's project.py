aa3e514f0dcacda1cc1f9f87dcdc74c2fc35589b
gagavastic <bo.sutormin@gmail.com>
2025-03-28 22:35:41 +0300

    продвижение

--- gagavastic's project.py
+++ gagavastic's project.py
@@ -1,12 +1,18 @@
 class my_class(object):
-    pass
-import random
+    def vvod_chisla():
+        while True:
+            a = int(input())
+            if 1 < a < 4:
+                return a
+            else:
+                print('Возможно вы немного ошиблись. Попробуйте ещё раз')
 
-print(random.randint(1, 100))
 harakteristika = dict()
 ochki = dict()
 summa_ochkov = 0
-print('Добрый день.')
+print('Добрый день. Сейча')
 
 
 
+print('Можете ли вы назвать себя усидчивым ччеловеком?')
+ochki['usidchivost'] = vvod_chisla
